483951d918736365beb6f3c628812d59c68fac1f
Mona Lisa <sysmic@sysmic.cl>
2025-05-31 19:34:05 -0400

    arreglado protobuf compiler, librerias varias, y settings de vscode

--- src/receivers/vision.hpp
+++ src/receivers/vision.hpp
@@ -8,9 +8,9 @@
 #include <utility> // for std::pair
 #include "../protobuf/ssl_vision_wrapper.pb.h"
 #include "../protobuf/ssl_vision_detection.pb.h"
-#include "robotstate.hpp"
-#include "robotkalman.hpp"
-#include "robotstatscollector.hpp"
+#include "../utilities/robotstate.hpp"
+#include "kalman/robotkalman.hpp"
+#include "kalman/robotstatscollector.hpp"
 
 
 

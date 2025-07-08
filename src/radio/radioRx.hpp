88341008255b19d1deed3e80d05bd438a3cbc39b
Mona Lisa <sysmic@sysmic.cl>
2025-07-07 21:03:13 -0400

    Radio RX added

---
+++ src/radio/radioRx.hpp
@@ -0,0 +1,43 @@
+// radioRx.hpp
+#ifndef RADIORX_HPP
+#define RADIORX_HPP
+
+#include <QObject>
+#include <QHash>
+#include <QDebug>
+#include <QSerialPort>
+#include <QFile>
+#include <QTimer>
+#include <QVector4D>
+#include "robotcommand.hpp"
+#include "grsim.hpp"
+
+class RadioRx : public QObject{
+    Q_OBJECT
+public:
+    /// Constructor
+    /// @param portName  Nombre del puerto (ej. "COM5" o "/dev/ttyUSB0")
+    /// @param baudRate  Baud rate (ej. QSerialPort::Baud460800)
+    RadioRx(const QString &portName = "COM6", qint32 baudRate = QSerialPort::Baud460800);
+
+    ~RadioRx();
+
+    struct RobotRxData {
+        int id;
+        bool ballDetected;
+        float wheelSpeeds[4];
+    };
+
+    // Devuelve el último paquete recibido para cada robot
+    QHash<int, RobotRxData> getLatestRobotData() const;
+
+    void readSerial();
+
+private:
+    QSerialPort serialPort;
+    qint32 m_baudRate;
+    QString m_portName;
+    QHash<int, RobotRxData> latestRobotData;
+};
+
+#endif // RADIORX_HPP
